1a30ebe0228ca74627a1da6fc25946846992ce5c
VarshaRaviAD <varsha.ravi@acceldata.io>
2025-04-11 18:24:56 +0530

    Update tpcds-build.sh

--- tpcds-build.sh
+++ tpcds-build.sh
@@ -13,9 +13,9 @@ done
 which mvn > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	SKIP=0
-	if [ -e "apache-maven-3.0.5-bin.tar.gz" ]; then
-		SIZE=`du -b apache-maven-3.0.5-bin.tar.gz | cut -f 1`
-		if [ $SIZE -eq 5144659 ]; then
+	if [ -e "apache-maven-3.9.9-bin.tar.gz" ]; then
+		SIZE=`du -b apache-maven-3.9.9-bin.tar.gz | cut -f 1`
+		if [ $SIZE -eq 19296 ]; then
 			SKIP=1
 		fi
 	fi
@@ -27,9 +27,9 @@ if [ $? -ne 0 ]; then
 			exit 1
 		fi
 	fi
-	tar -zxf apache-maven-3.0.5-bin.tar.gz > /dev/null
+	tar -zxf apache-maven-3.9.9-bin.tar.gz > /dev/null
 	CWD=$(pwd)
-	export MAVEN_HOME="$CWD/apache-maven-3.0.5"
+	export MAVEN_HOME="$CWD/apache-maven-3.9.9"
 	export PATH=$PATH:$MAVEN_HOME/bin
 fi
 
